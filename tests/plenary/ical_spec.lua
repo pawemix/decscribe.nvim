69f2a03040adba307c953f25886d403fb425919a
pawemix <26186778+pawemix@users.noreply.github.com>
2024-02-04 23:00:49 +0100

    test: add a case checking pointing at the 1st char of the prop name

--- tests/plenary/ical_spec.lua
+++ tests/plenary/ical_spec.lua
@@ -5,16 +5,23 @@ local ic = require("decscribe.ical")
 local eq = assert.are_same
 
 describe("find_ical_prop", function()
-	it("finds description in the middle of a one-prop ical", function()
-		local data = table.concat({
-			"BEGIN:CALENDAR",
-			"BEGIN:VTODO",
-			"DESCRIPTION:something",
-			"END:VTODO",
-			"END:CALENDAR",
-		}, "\r\n") .. "\r\n"
+	local one_prop_data = table.concat({
+		"BEGIN:CALENDAR", -- 14 chars + 2 ("\r\n") = 16
+		"BEGIN:VTODO", -- 11 chars + 2 ("\r\n") = 13
+		"DESCRIPTION:something",
+		"END:VTODO",
+		"END:CALENDAR",
+	}, "\r\n") .. "\r\n"
 
-		eq("something", ic.find_ical_prop(data, "DESCRIPTION"))
+	it("finds description in the middle of a one-prop ical", function()
+		local prop, _, _, _ = ic.find_ical_prop(one_prop_data, "DESCRIPTION")
+		eq("something", prop)
+	end)
+
+	it("points at the first char of the prop name", function ()
+		local _, key_i, _, _ = ic.find_ical_prop(one_prop_data, "DESCRIPTION")
+		-- 16 + 13 + 1 ("\n" -> "D") = 30
+		eq(30, key_i)
 	end)
 
 	it("finds description in the middle of multi-prop ical", function()
