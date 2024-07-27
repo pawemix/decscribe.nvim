f5b85cd4665d6790a5fdd69bc881e45bbead444c
pawemix <26186778+pawemix@users.noreply.github.com>
2024-07-27 20:46:31 +0200

    feat: add core type definitions

---
+++ lua/decscribe/core.lua
@@ -0,0 +1,9 @@
+
+---@class decscribe.core.Todo
+---@field completed boolean
+---@field summary string
+---@field subtasks? decscribe.core.Todo[]
+
+---@alias decscribe.core.Todos
+---| decscribe.mixcoll.MixColl<string, decscribe.core.Todo>
+
