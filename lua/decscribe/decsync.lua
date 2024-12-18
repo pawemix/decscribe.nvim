3847125df8087a7439e547c23808d054b334df67
pawemix <26186778+pawemix@users.noreply.github.com>
2024-12-18 19:12:03 +0100

    feat: support ical update batches; retrieve entries from all apps' dirs

--- lua/decscribe/decsync.lua
+++ lua/decscribe/decsync.lua
@@ -1,3 +1,5 @@
+local os = require("os")
+
 local M = {}
 
 ---@param shell_code string
@@ -19,6 +21,28 @@ local function run(shell_code, ...)
 	end
 	prog:close()
 	return lines
+end
+
+---@param shell_code string
+---@vararg string
+---@return fun(): string? next
+local function run_iter(shell_code, ...)
+	local args = { ... }
+	for idx, arg in ipairs(args) do
+		args[idx] = "'" .. arg:gsub("'", "'\\''") .. "'"
+	end
+	shell_code = "'" .. shell_code:gsub("'", "'\\''") .. "'"
+	table.insert(args, 1, "sh")
+	table.insert(args, 1, "")
+	local prog = io.popen("sh -c " .. shell_code .. table.concat(args, " "), "r")
+	if not prog then return function() return nil end end
+	local lines_next = prog:lines()
+	return function()
+		local next = lines_next()
+		if next then return next end
+		prog:close()
+		return nil
+	end
 end
 
 ---Check the filesystem whether `path` is a decsync directory.
@@ -70,25 +94,50 @@ function M.list_task_colls(ds_dir)
 end
 
 ---@param ds_dir string
----@param app_id string
 ---@param coll_id string
 ---@return { [string]: string }? uids_to_icals
-function M.retrieve_task_icals(ds_dir, app_id, coll_id)
+function M.retrieve_task_icals(ds_dir, coll_id)
 	ds_dir = ds_dir:gsub("/*$", "", 1)
-	local bucket_jsons =
-		run('cat "$1"/tasks/"$2"/v2/"$3"/??', ds_dir, coll_id, app_id)
-	if not bucket_jsons then return nil end
-	local uids_to_icals = {}
-	local buckets = vim.json.decode(
-		"[" .. table.concat(bucket_jsons, ",") .. "]",
-		{ luanil = { object = true, array = true } }
-	)
-	for _, bucket in ipairs(buckets) do
-		local uid = bucket[1][2]
-		local ical = bucket[4]
-		if ical then uids_to_icals[uid] = ical end
+	local uid_to_updated_datetime = {}
+	local uid_to_ical = {}
+	for entry_str in run_iter('cat "$1"/tasks/"$2"/v2/*/??', ds_dir, coll_id) do
+		local entry = assert(
+			vim.json.decode(entry_str, { luanil = { object = true, array = true }}),
+			"Could not JSON-parse an entry: " .. vim.inspect(entry_str)
+		)
+		local uid = assert(entry[1][2])
+		local updated_datetime = assert(
+			entry[2], "entry[2]: string expected; got " .. vim.inspect(entry[2]))
+		local ical_or_nil = entry[4]
+		assert(
+			type(ical_or_nil) == "string" or type(ical_or_nil) == "nil",
+			"Unexpected type of ical_or_nil: " .. type(ical_or_nil))
+		-- Use current todo replica if
+		-- it's not there yet or previously read replica was older:
+		if
+			not uid_to_updated_datetime[uid]
+			or uid_to_updated_datetime[uid] < updated_datetime
+		then
+			uid_to_ical[uid] = ical_or_nil
+			uid_to_updated_datetime[uid] = updated_datetime
+		end
 	end
-	return uids_to_icals
+	return uid_to_ical
+end
+
+---Get a bucket with the least amount of lines.
+---@param t { [BucketPath]: integer }
+---@return BucketPath?
+local function get_by_least_count(t)
+	local min_count = math.huge
+	local min_bucket = nil
+	for bucket_path, count in pairs(t) do
+		if count < min_count then
+			min_count = count
+			min_bucket = bucket_path
+		end
+	end
+	return min_bucket
 end
 
 ---@alias decscribe.decsync.Uid string
@@ -97,14 +146,85 @@ end
 ---@param app_id string
 ---@param coll_id string
 ---@param ical_updates { [decscribe.decsync.Uid]: string|false }
----@return boolean success
-function M.update_task_ical(ds_dir, app_id, coll_id, ical_updates)
-	--- TODO rebuild into many ical updates/creations/removals at once
+function M.patch_task_icals(ds_dir, app_id, coll_id, ical_updates)
 	local bucket_paths =
 		run('ls -1d "$1"/tasks/"$2"/v2/"$3"/??', ds_dir, coll_id, app_id)
+	---@alias BucketPath string
+	---@type { [BucketPath]: integer }
+	local bucket_counts = {}
+	assert(bucket_paths, "Couldn't read decsync task file entries!")
 	for _, bucket_path in ipairs(bucket_paths) do
+		local bucket_file_ro = io.open(bucket_path, "r")
+		assert(bucket_file_ro, ("Couldn't open '%s'!"):format(bucket_path))
+		local bucket_lines = {}
+		for entry_line in bucket_file_ro:lines("*l") do
+			bucket_lines[#bucket_lines + 1] = entry_line
+		end
+		bucket_file_ro:close()
 
-	end
+		local bucket_changed = false
+		for idx, entry_line in ipairs(bucket_lines) do
+			local entry = assert(
+				vim.json.decode(
+					entry_line,
+					{ luanil = { object = true, array = true } }
+				)
+			)
+			local uid = entry[1][2]
+			-- Don't update anything if no changes to the todo with this UID:
+			if ical_updates[uid] == nil then goto continue_bucket_lines end
+			-- Bump last update datetime (ISO8601 w/o TZ info in UTC Zero):
+			entry[2] = os.date("!%Y-%m-%dT%H:%M:%S")
+			-- Update the entry
+			-- NOTE: vim.v.null has to be used instead of nil, otherwise JSON won't
+			-- render the object key / array element at all if possible.
+			entry[4] = ical_updates[uid] or vim.v.null
+			bucket_lines[idx] = assert(vim.json.encode(entry))
+			ical_updates[uid] = nil
+			bucket_changed = true
+			--
+			::continue_bucket_lines::
+		end -- for bucket_lines
+		bucket_counts[bucket_path] = #bucket_lines
+		-- Rewrite the bucket with new lines if anything changed:
+		if bucket_changed then
+			local bucket_file_wo = assert(
+				io.open(bucket_path, "w"),
+				("Couldn't open '%s' for writing!"):format(bucket_path)
+			)
+			bucket_file_wo:write(table.concat(bucket_lines, "\n") .. "\n")
+			bucket_file_wo:close()
+		end -- if bucket_changed
+	end -- for bucket_paths
+
+	-- Remaining ical updates are NEW entries - insert them:
+	for uid, ical_or_false in pairs(ical_updates) do
+		assert(
+			type(ical_or_false) == "string" or ical_or_false == false,
+			"Unexpected type of ical_or_false: " .. type(ical_or_false))
+		local sel_bucket_path =
+			assert(get_by_least_count(bucket_counts)) -- no buckets is unexpected
+		local sel_bucket_file = assert(
+			io.open(sel_bucket_path, "a"),
+			("Couldn't open '%s' file for appending!"):format(sel_bucket_path)
+		)
+		local entry = {
+			{ "resources", uid },
+			os.date("!%Y-%m-%dT%H:%M:%S"),
+			vim.v.NIL,
+			ical_or_false or vim.v.NIL
+		}
+		local entry_json = assert(
+			vim.json.encode(entry),
+			"Entry could not be encoded as JSON: " .. vim.inspect(entry)
+		)
+		sel_bucket_file:write(entry_json .. "\n")
+		assert(
+			sel_bucket_file:close(),
+			("Couldn't close file '%s'!"):format(sel_bucket_path)
+		)
+		bucket_counts[sel_bucket_path] = bucket_counts[sel_bucket_path] + 1
+	end -- ical creations
 end
 
 return M
